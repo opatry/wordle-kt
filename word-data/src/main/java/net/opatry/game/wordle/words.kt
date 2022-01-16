b6e0be2644704aabe26985ce4292942a8968f2b9
Olivier Patry <olivier.patry@gmail.com>
2022-01-16 20:37:18 +0100

    More readable words loading

--- word-data/src/main/java/net/opatry/game/wordle/words.kt
+++ word-data/src/main/java/net/opatry/game/wordle/words.kt
@@ -25,4 +25,7 @@ package net.opatry.game.wordle
 object Words
 
 // source: https://github.com/octokatherine/word-master/blob/58a4534e58a058e3fbccfcc99c881992d319b159/src/data/words.js
-val words = Words::class.java.getResource("/words.txt")?.readText()?.split(Regex("\\s")) ?: emptyList()
+val words = Words::class.java.getResource("/words_en_5.txt")
+    ?.readText()
+    ?.lines()
+    ?: emptyList()
