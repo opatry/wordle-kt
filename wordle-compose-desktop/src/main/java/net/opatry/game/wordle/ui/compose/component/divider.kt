a954a768886afb32643afe7c936a9b786ff383ed
Olivier Patry <olivier.patry@gmail.com>
2022-01-22 00:47:27 +0100

    Add vertical divider component

---
+++ wordle-compose-desktop/src/main/java/net/opatry/game/wordle/ui/compose/component/divider.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022 Olivier Patry
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package net.opatry.game.wordle.ui.compose.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+
+// Duplicated from original horizontal Divider
+
+/**
+ * <a href="https://material.io/components/dividers" class="external" target="_blank">Material Design divider</a>.
+ *
+ * A divider is a thin line that groups content in lists and layouts.
+ *
+ * ![Dividers image](https://developer.android.com/images/reference/androidx/compose/material/dividers.png)
+ *
+ * @param color color of the divider line
+ * @param thickness thickness of the divider line, 1 dp is used by default. Using [Dp.Hairline]
+ * will produce a single pixel divider regardless of screen density.
+ * @param startIndent start offset of this line, no offset by default
+ */
+@Composable
+fun VerticalDivider(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.onSurface.copy(alpha = DividerAlpha),
+    thickness: Dp = 1.dp,
+    startIndent: Dp = 0.dp
+) {
+    val indentMod = if (startIndent.value != 0f) {
+        Modifier.padding(start = startIndent)
+    } else {
+        Modifier
+    }
+    val targetThickness = if (thickness == Dp.Hairline) {
+        (1f / LocalDensity.current.density).dp
+    } else {
+        thickness
+    }
+    Box(
+        modifier.then(indentMod)
+            .fillMaxHeight()
+            .width(targetThickness)
+            .background(color = color)
+    )
+}
+
+private const val DividerAlpha = 0.12f
