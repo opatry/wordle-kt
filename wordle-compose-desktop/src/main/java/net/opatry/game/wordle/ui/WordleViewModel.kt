399dfee6b0a09b119e2287c10683566f0899df77
Olivier Patry <olivier.patry@gmail.com>
2022-01-22 00:48:26 +0100

    Fix reset of time to beginning of the day

--- wordle-compose-desktop/src/main/java/net/opatry/game/wordle/ui/WordleViewModel.kt
+++ wordle-compose-desktop/src/main/java/net/opatry/game/wordle/ui/WordleViewModel.kt
@@ -291,7 +291,7 @@ class WordleViewModel(
                 set(Calendar.HOUR, 0)
                 set(Calendar.MINUTE, 0)
                 set(Calendar.SECOND, 0)
-                set(Calendar.MILLISECOND, 0)
+                set(Calendar.MILLISECOND, 1)
                 val today = timeInMillis
                 // reset date to Wordle t0 at 00:00:00.000
                 set(Calendar.YEAR, 2021)
